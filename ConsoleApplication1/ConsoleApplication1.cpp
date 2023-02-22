e3d13a1475815e3b8ae6714c3462c21cd8444dce
Marek Pałkowski <mpalkowski@zut.edu.pl>
2023-02-22 10:29:48 +0100

    vector

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,18 +4,23 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector> 
 
 using namespace std;
 
 int main()
 {
+    vector<string> linie;
 
     ofstream MyFile("filename.txt");
     ifstream MyFile2("filename2.txt");
     string data; 
 
-    MyFile << "ok!!\n";
-    MyFile << "second!!";
+    linie.push_back("1. Pierwsza linia");
+    linie.push_back("2. Druga linia");
+
+    for (auto linia : linie)
+        MyFile << linia << endl;
 
     while (!MyFile2.eof())
     {
